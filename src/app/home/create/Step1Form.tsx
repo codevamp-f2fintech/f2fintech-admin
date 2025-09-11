72807a26f95fb4f76d9c63cd5227a339bee86bfe
shnawaz844 <your_correct_email>
2025-09-11 17:42:43 +0530

    loan tenure increased

--- src/app/home/create/Step1Form.tsx
+++ src/app/home/create/Step1Form.tsx
@@ -823,7 +823,7 @@ const Step1Form: React.FC<Step1FormProps> = ( {
               },
             }}
           >
-            {[ "3 Years", "5 Years", "8 Years" ].map( ( label ) => (
+            {[ "3 Years", "5 Years", "8 Years","10 Years","15 Years","20 Years","25 Years","30 Years" ].map( ( label ) => (
               <MenuItem
                 key={label}
                 value={label}
