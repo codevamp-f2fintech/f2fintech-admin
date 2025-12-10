01ec7d5bdf22e59d0733c982d94621bd514f3da5
shnawaz844 <your_correct_email>
2025-12-10 13:20:56 +0530

    side nav for ipad pro made responsive 3

--- src/app/components/dashboard/layout/side-nav.tsx
+++ src/app/components/dashboard/layout/side-nav.tsx
@@ -56,9 +56,14 @@ export function SideNav (): React.JSX.Element {
         position: "sticky",
         scrollbarWidth: "none",
         top: 0,
-        width: collapsed ? "5vw" : "15vw",
+        width: collapsed ? "5.5vw" : "15vw",
         zIndex: "var(--SideNav-zIndex)",
         transition: "width 0.3s",
+        // iPad Pro 12.9" (1024 x 1366)
+        "@media only screen and (min-width: 1024px) and (max-width: 1366px) and (orientation: landscape)": {
+          width: collapsed ? "100px" : "220px",
+        },
+      
         "&::-webkit-scrollbar": { display: "none" },
       }}
     >
@@ -231,7 +236,13 @@ function NavItem ( {
             flex: "0 0 auto",
             height: "5vh",
             width: "3vw",
-            borderRadius: "50px",
+            borderRadius: "20px",
+            // iPad Pro 12.9" landscape
+            "@media only screen and (min-width: 1024px) and (max-width: 1366px) and (orientation: landscape)": {
+              height: "50px",
+              width: "50px",
+            },
+           
             "&:hover": {
               transform: "scale(1.1)",
               background:
